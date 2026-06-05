c5db2fd05cbdd4eccaf9c4da68127aac8f39b517
FreeTeaspoon <76246708+FreeTeaspoon@users.noreply.github.com>
2026-06-06 00:18:14 +1000

    Initial DuckDuckGo Email Raycast extension

---
+++ raycast-env.d.ts
@@ -0,0 +1,33 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {
+  /** Access Token - DuckDuckGo Email Protection access token. Leave empty to sign in from the command. */
+  "accessToken"?: string,
+  /** Duck Address - Your main Duck address without @duck.com. Used for one-time passphrase login. */
+  "duckAddress"?: string
+}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `generate-alias` command */
+  export type GenerateAlias = ExtensionPreferences & {}
+  /** Preferences accessible in the `generate-copy-alias` command */
+  export type GenerateCopyAlias = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `generate-alias` command */
+  export type GenerateAlias = {}
+  /** Arguments passed to the `generate-copy-alias` command */
+  export type GenerateCopyAlias = {}
+}
+
